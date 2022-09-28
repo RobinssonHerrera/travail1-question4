af3e6a8faff74770d67fc8b4fccf2be8ad950311
Robinsson  Herrera <2035709@cegepthetford.ca>
2022-09-28 13:37:54 -0400

    Modification de myCode.js

--- myCode.js
+++ myCode.js
@@ -1,19 +1,23 @@
-function addition(a, b) {
+function mystere(a, b){
+  return a + a + a;
+}
+
+function addition(a, b){
   return a + b
 }
 
-function multiplication(a, b) {
+function multiplication(a, b){
   return a * b;
 }
 
-function mystere(a, b) {
-  return a + a + a;
+function division(a,b){
+  return a/b;
 }
 
 function print(operation, a, b) {
   console.log(operation(a, b));
 }
 
-print(addition, 4, 4);
 print(multiplication, 4, 4);
 print(mystere, 2, 4);
+print(addition, 3, 4);
